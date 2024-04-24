12b0584dc5defc4159e64a1e70884758d55b23b6
crob127 <164558528+crob127@users.noreply.github.com>
2024-04-23 23:29:12 -0400

    set up HTML and file pathing

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// step 1 build structure of landing page in HTML
+// step 2 create inputs for username, title, content, and a submit button
+// step 3 make the inputs submitable, if all 3 inputs are not filled out then return an error message
+// step 4 save the post information to local storage
+// step 5 display local storage information with a JSON array of all blog post inputs
+// step 6 create a second page that shows all blog posts, pulled from local storage
+// step 7 provide navigation back to the main page
+// step 8 provide a light mode/dark mode toggle on the header of the second page
+// step 9 include a link to my portfolio in the footer
+// step 10 
